01c96bddadfb0db6e283d454b6842bbf5b0b4728
Kushal <kushaldazzle@gmail.com>
2021-05-17 16:05:53 +0530

    added more functionailty to authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, RegisterView, ChangePassword, UpdateUserName
 
 
 
 urlpatterns = [
     path("login/" , LoginView.as_view()),
+    path('register/', RegisterView.as_view()),
+    path('changepassword/', ChangePassword.as_view()),
+    path('updateusername/', UpdateUserName.as_view()),
 ]
