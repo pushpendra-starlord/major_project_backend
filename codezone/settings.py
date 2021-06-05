4584ed33186dea5f26b2e16720495ee323ae400c
Kushal <kushaldazzle@gmail.com>
2021-06-05 13:31:54 +0530

    changes for heroku

--- codezone/settings.py
+++ codezone/settings.py
@@ -110,12 +110,19 @@ WSGI_APPLICATION = 'codezone.wsgi.application'
 
 # Channels config
 ASGI_APPLICATION = 'codezone.routing.application'
+# CHANNEL_LAYERS = {
+#         "default": {
+#             "BACKEND": "channels.layers.InMemoryChannelLayer"
+#         }
+#     }
 CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "channels.layers.InMemoryChannelLayer"
+        'default': {
+            'BACKEND': 'channels_redis.core.RedisChannelLayer',
+            'CONFIG': {
+                "hosts": [('127.0.0.1', 6379)],
+            }
         }
     }
-
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
