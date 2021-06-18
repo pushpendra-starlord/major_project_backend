3acfff0ee64360c61999b8f9148ad4a809b18fb4
Kushal <kushaldazzle@gmail.com>
2021-06-18 20:10:30 +0530

    changes in urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from blog.views import BlogPostView,LikeView,CommentView,ListBlogView
+from blog.views import BlogPostView,LikeView,CommentView,ListBlogView, UnlikePost
 
 urlpatterns=[
     path("post/list/",ListBlogView.as_view()),
     path("post/",BlogPostView.as_view()),
     path("like/<int:id>/",LikeView.as_view()),
     path("comment/<int:id>/",CommentView.as_view()),
+    path("comment/",CommentView.as_view()),
+    path('unlike/' , UnlikePost.as_view())
    
 ]
